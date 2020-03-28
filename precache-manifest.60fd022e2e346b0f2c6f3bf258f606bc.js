348802c09a20900abce5a41e0ac394c0a5fd5cfc
Naranov Naran <na.naranov@gmail.com>
2020-03-28 22:56:00 +0300

    Updates

---
+++ precache-manifest.60fd022e2e346b0f2c6f3bf258f606bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85a0b18bcdedb2c9fa83b31cbe768e5",
+    "url": "/search_films_reactJS/index.html"
+  },
+  {
+    "revision": "997194ef07adafda6b97",
+    "url": "/search_films_reactJS/static/css/main.79f46164.chunk.css"
+  },
+  {
+    "revision": "6de24bfe50d851913335",
+    "url": "/search_films_reactJS/static/js/2.1d23e7bb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/search_films_reactJS/static/js/2.1d23e7bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997194ef07adafda6b97",
+    "url": "/search_films_reactJS/static/js/main.1d4bb50f.chunk.js"
+  },
+  {
+    "revision": "3c6fd0f81f6a40638fec",
+    "url": "/search_films_reactJS/static/js/runtime-main.613e886a.js"
+  }
+]);
